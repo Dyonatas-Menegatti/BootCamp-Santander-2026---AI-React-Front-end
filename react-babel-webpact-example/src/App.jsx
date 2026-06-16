3136339a3383ed3417b2091f93f0512dff2dbf55
Dyonatas Menegatti <dionatas_fm@hotmail.com>
2026-06-16 17:18:18 -0300

    Estou iniciando meus primeiros projetos em React. Este repositório tem como objetivo registrar minha evolução com o React e demonstrar meu progresso no uso de diferentes bibliotecas.

---
+++ react-babel-webpact-example/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import App from "./App";
+
+const App = () => {
+    return (
+        <div>
+            <h1>Meu Projeto React</h1>
+            <p>Bem-vindo ao meu aplicativo React!</p>
+        </div>
+    );
+};
